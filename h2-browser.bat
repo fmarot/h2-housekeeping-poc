9bdcbe7153a6d9fdb69f833c47d57f544dc3bf7d
Francois Marot <francois.marot@olea-medical.com>
2024-08-20 13:02:28 +0200

    h2 v 2.3.232

--- h2-browser.bat
+++ h2-browser.bat
@@ -1,6 +1,6 @@
 cd %~dp0
 
-java.exe -jar "target\lib\h2-2.3.230.jar" ^
+java.exe -jar "target\lib\h2-2.3.232.jar" ^
 	-url "jdbc:h2:.\\target\\DB\\poc;TRACE_LEVEL_FILE=4"  ^
 	-user login  ^
 	-password pass ^
